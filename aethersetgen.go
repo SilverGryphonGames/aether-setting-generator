3f7a3064aaea8395d6f4c9c7457e69b4e0f8661c
Ben Overmyer <ben.overmyer@protolabs.com>
2019-04-26 09:48:19 -0500

    Add description

--- aethersetgen.go
+++ aethersetgen.go
@@ -2,10 +2,11 @@ package aethersetgen
 
 // Setting is a conglomeration of descriptors for a game setting
 type Setting struct {
-	TimePeriod TimePeriod
-	Technology Technology
-	Tone       Tone
-	Genres     []Genre
+	Description string
+	TimePeriod  TimePeriod
+	Technology  Technology
+	Tone        Tone
+	Genres      []Genre
 }
 
 // Generate generates a random setting
@@ -15,6 +16,15 @@ func Generate() Setting {
 	setting.Technology = getRandomTechnology()
 	setting.Tone = getRandomTone()
 	setting.Genres = getRandomGenres()
+	setting.Description = setting.getDescription()
 
 	return setting
+}
+
+func (setting Setting) getDescription() string {
+	var description string
+
+	description = "In a " + setting.TimePeriod.Name + " time period, using " + setting.Technology.Name + " technology. The tone is " + setting.Tone.Name + ", and the setting fits the " + setting.Genres[0].Name + " and " + setting.Genres[1].Name + " genres."
+
+	return description
 }
